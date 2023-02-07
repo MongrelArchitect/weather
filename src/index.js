698f9a693d3eb312696d20a6f60f4cec795f1fb6
Sean Eric Thomas <set@seanericthomas.com>
2023-02-07 14:24:46 -0800

    Display searched weather data

--- src/index.js
+++ src/index.js
@@ -13,8 +13,33 @@ function processData(data) {
     temp: data.main.temp,
     coord: data.coord,
     clouds: data.clouds.all,
-    wind: data.wind,
+    wind: data.wind.speed,
   };
 }
 
-getWeather('Eureka').then((result) => console.log(processData(result)));
+function drawWeather(data) {
+  const cityName = document.querySelector('.city-name');
+  const weatherName = document.querySelector('.weather-name');
+  const temp = document.querySelector('.temp');
+  const weatherDesc = document.querySelector('.weather-desc');
+  const clouds = document.querySelector('.clouds');
+  const wind = document.querySelector('.wind');
+
+  cityName.textContent = data.name;
+  weatherName.textContent = data.weather.name;
+  temp.textContent = `${data.temp} °F`;
+  weatherDesc.textContent = data.weather.description;
+  clouds.textContent = `${data.clouds}% cloud cover`;
+  wind.textContent = `Wind speed: ${data.wind}mph`;
+}
+
+function searchCity() {
+  const searchButton = document.querySelector('button');
+  searchButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const city = document.querySelector('input').value;
+    getWeather(city).then((result) => drawWeather(processData(result)));
+  });
+}
+
+searchCity();
